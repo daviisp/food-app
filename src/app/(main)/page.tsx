e26493210ec78696ae61f3c057558df7963e4f2c
Davi Silva <davi.pimenta8@gmail.com>
2025-01-14 21:31:07 -0300

    refactor: move page.tsx to (main) structure

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import { SearchRestaurant } from "./_components/search-restaurant";
+
+const Home = () => {
+  return (
+    <>
+      <section className="px-5 py-6">
+        <SearchRestaurant />
+      </section>
+    </>
+  );
+};
+
+export default Home;

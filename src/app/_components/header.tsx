36ca846401150f0e89ad1eba2bb2bdf4dfa0586f
Davi Silva <davi.pimenta8@gmail.com>
2025-01-20 20:39:58 -0300

    style: adjust margin top in header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,7 +5,7 @@ import { UserProfile } from "./user-profile";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between px-5 pt-6">
+    <header className="mt-6 flex items-center justify-between px-5">
       <Link href="/">
         <Image
           src="/logo.svg"

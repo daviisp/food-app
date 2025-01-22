82c324466598965142412a37f9aefbd8d8f56338
Davi Silva <davi.pimenta8@gmail.com>
2025-01-22 09:45:27 -0300

    feat: create a helper to calculate discount of product with quantity

--- src/helpers/price.ts
+++ src/helpers/price.ts
@@ -4,6 +4,11 @@ interface ProductWithNumberPrice extends Omit<Product, "originalPrice"> {
   originalPrice: number;
 }
 
+interface ProductWithQuantity extends Omit<Product, "originalPrice"> {
+  originalPrice: number;
+  quantity: number;
+}
+
 export const formatPrice = (price: number) => {
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -16,4 +21,14 @@ export const calculateDiscountProduct = (product: ProductWithNumberPrice) => {
     (Number(product.originalPrice) * product.discountPercentage) / 100;
 
   return Number(product.originalPrice) - discount;
+};
+
+export const calculateDiscountProductWithQuantity = ({
+  originalPrice,
+  discountPercentage,
+  quantity,
+}: ProductWithQuantity) => {
+  const discount = (Number(originalPrice) * discountPercentage) / 100;
+
+  return (Number(originalPrice) - discount) * quantity;
 };

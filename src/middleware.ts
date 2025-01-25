1c843e0420a8804f0336e3a50938c8a5a9b62ce4
Davi Silva <davi.pimenta8@gmail.com>
2025-01-25 13:30:42 -0300

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getUrl } from "./helpers/get-url";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("__Secure-authjs.session-token");
+  const pathname = request.nextUrl.pathname;
+
+  if (
+    (!token && pathname === "/my-orders") ||
+    (!token && pathname === "my-favorites-restaurants")
+  ) {
+    return NextResponse.redirect(new URL(getUrl("/")));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};

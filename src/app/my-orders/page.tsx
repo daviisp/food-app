d4f6424e5acd591bc2d6bd5684268eb104efb86c
Davi Silva <davi.pimenta8@gmail.com>
2025-01-25 11:37:03 -0300

    feat: add logic to display non-finalized orders first

--- src/app/my-orders/page.tsx
+++ src/app/my-orders/page.tsx
@@ -7,13 +7,27 @@ import Link from "next/link";
 import { ChevronRight } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { formatPrice } from "@/helpers/price";
+import { format, isBefore, startOfToday } from "date-fns";
 
-const MyOders = async () => {
+const MyOrders = async () => {
   const orders = await getOrdersByUser();
 
   if (!orders) {
     return;
   }
+
+  const today = startOfToday();
+
+  const sortedOrders = orders.sort((a, b) => {
+    const aDate = new Date(a.createdAt);
+    const bDate = new Date(b.createdAt);
+
+    const aCompleted = isBefore(aDate, today);
+    const bCompleted = isBefore(bDate, today);
+
+    if (aCompleted === bCompleted) return 0;
+    return aCompleted ? 1 : -1;
+  });
 
   return (
     <>
@@ -21,57 +35,74 @@ const MyOders = async () => {
       <div>
         <h2 className="px-5 py-6 text-lg font-semibold">Meus Pedidos</h2>
         <div className="space-y-2">
-          {orders.map((order) => (
-            <Card key={order.id} className="mx-5">
-              <CardContent className="p-5">
-                <div>
-                  <div className="w-fit rounded-xl bg-[#5DC05B] px-2 py-0.5 text-xs font-semibold text-white">
-                    <span>Em transporte</span>
-                  </div>
-                </div>
+          {sortedOrders.map((order) => {
+            const orderDate = new Date(order.createdAt);
+            const isOrderCompleted = isBefore(orderDate, today);
 
-                <div className="flex items-center justify-between py-3">
-                  <div className="flex items-center gap-2">
-                    <Image
-                      src={order.product.imageUrl}
-                      alt={order.product.name}
-                      width={16}
-                      height={16}
-                      className="h-6 w-6 rounded-full object-cover"
-                      unoptimized
-                    />
-                    <p className="text-sm font-semibold">
-                      {order.product.restaurant.name}
+            return (
+              <Card key={order.id} className="mx-5">
+                <CardContent className="p-5">
+                  <div>
+                    <div
+                      className={`w-fit rounded-xl px-2 py-0.5 text-xs font-semibold text-white ${
+                        isOrderCompleted ? "bg-gray-500" : "bg-[#5DC05B]"
+                      }`}
+                    >
+                      <span>
+                        {isOrderCompleted ? "Finalizado" : "Em transporte"}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center justify-between py-3">
+                    <div className="flex items-center gap-2">
+                      <Image
+                        src={order.product.imageUrl}
+                        alt={order.product.name}
+                        width={16}
+                        height={16}
+                        className="h-6 w-6 rounded-full object-cover"
+                        unoptimized
+                      />
+                      <p className="text-sm font-semibold">
+                        {order.product.restaurant.name}
+                      </p>
+                    </div>
+                    <div>
+                      <Button
+                        asChild
+                        className="hover:bg-white"
+                        variant="ghost"
+                      >
+                        <Link
+                          href={`/restaurants/${order.product.restaurant.id}`}
+                        >
+                          <ChevronRight size={20} />
+                        </Link>
+                      </Button>
+                    </div>
+                  </div>
+                  <Separator />
+                  <div className="flex items-center gap-1.5 py-3">
+                    <span className="flex h-[18px] w-[18px] items-center justify-center rounded-full bg-[#7E8392] text-sm text-white">
+                      {order.quantity}
+                    </span>
+                    <p className="text-sm text-[#7E8392]">
+                      {order.product.name}
                     </p>
                   </div>
-                  <div>
-                    <Button asChild className="hover:bg-white" variant="ghost">
-                      <Link
-                        href={`/restaurants/${order.product.restaurant.id}`}
-                      >
-                        <ChevronRight size={20} />
-                      </Link>
-                    </Button>
+                  <Separator />
+                  <div className="pt-3">
+                    <p className="text-xs">{formatPrice(order.price)}</p>
                   </div>
-                </div>
-                <Separator />
-                <div className="flex items-center gap-1.5 py-3">
-                  <span className="flex h-[18px] w-[18px] items-center justify-center rounded-full bg-[#7E8392] text-sm text-white">
-                    {order.quantity}
-                  </span>
-                  <p className="text-sm text-[#7E8392]">{order.product.name}</p>
-                </div>
-                <Separator />
-                <div className="pt-3">
-                  <p className="text-xs">{formatPrice(order.price)}</p>
-                </div>
-              </CardContent>
-            </Card>
-          ))}
+                </CardContent>
+              </Card>
+            );
+          })}
         </div>
       </div>
     </>
   );
 };
 
-export default MyOders;
+export default MyOrders;

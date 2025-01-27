68e9d28507762318946e97e2dfea3d6ceafffc78
Davi Silva <davi.pimenta8@gmail.com>
2025-01-26 21:25:50 -0300

    fix: correct cookie access for session token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getUrl } from "./helpers/get-url";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("authjs.csrf-token");
+  const token = request.cookies.get("__Secure-authjs.session-token");
   const pathname = request.nextUrl.pathname;
 
   if (

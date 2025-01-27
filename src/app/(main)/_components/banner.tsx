c4b2267bb204beaf4934970d64997f98e7c556a7
Davi Silva <davi.pimenta8@gmail.com>
2025-01-27 13:45:02 -0300

    feat: create a hero section in desktop

--- src/app/(main)/_components/banner.tsx
+++ src/app/(main)/_components/banner.tsx
@@ -2,12 +2,19 @@ import Image from "next/image";
 
 interface BannerProps {
   src: string;
+  className?: string;
 }
 
-export const Banner = ({ src }: BannerProps) => {
+export const Banner = ({ src, className }: BannerProps) => {
   return (
-    <div className="relative h-40 w-full">
-      <Image src={src} alt="Até 30% de desconto em Pizzas" fill unoptimized />
+    <div className="relative h-40 w-full md:h-[371px] md:w-[371px]">
+      <Image
+        src={src}
+        alt="Até 30% de desconto em Pizzas"
+        fill
+        unoptimized
+        className={className}
+      />
     </div>
   );
 };
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,23 +1,51 @@
+import { Input } from "@/components/ui/input";
 import { Header } from "../_components/header";
 import { Banner } from "./_components/banner";
 import { Categories } from "./_components/categories";
 import { RecommendedProducts } from "./_components/recommended-products";
 import { RecommendedRestaurants } from "./_components/recommended-restaurants";
 import { SearchRestaurant } from "./_components/search-restaurant";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import { SearchRestaurantDesktop } from "./_components/search-restaurant-desktop";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <section className="px-5 pt-6">
+      <section className="px-5 pt-6 md:hidden">
         <SearchRestaurant />
       </section>
-      <section className="px-5 pt-6">
+      <section className="px-5 pt-6 md:hidden">
         <Categories />
       </section>
-      <section className="px-5 pt-6">
+      <section className="px-5 pt-6 md:hidden">
         <Banner src="/banner-discount.svg" />
       </section>
+      <section className="mt-5 hidden h-[500px] items-center justify-between bg-button px-32 md:flex">
+        <div className="w-[630px]">
+          <h2 className="mb-3 text-5xl font-bold text-white">Está com fome?</h2>
+          <p className="text-lg text-white">
+            Com apenas alguns cliques, encontre refeições acessíveis perto de
+            você.
+          </p>
+          <div className="mt-8 rounded-lg bg-white p-6">
+            <SearchRestaurantDesktop />
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/banner-desktop.png"
+            alt="Banner"
+            width={371}
+            height={371}
+            unoptimized
+          />
+        </div>
+      </section>
+
       <section className="px-5 pt-6">
         <RecommendedProducts />
       </section>

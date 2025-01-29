a6670d88b165994093da33c40f5a85e95b2eb62e
Davi Silva <davi.pimenta8@gmail.com>
2025-01-29 17:09:03 -0300

    feat: add responsive in category page

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -2,6 +2,7 @@ import { getCategory } from "@/actions/get-category";
 import { Header } from "../_components/header";
 import { notFound } from "next/navigation";
 import { ProductItem } from "../_components/product-item";
+import { Separator } from "@/components/ui/separator";
 
 interface CategoryPageParams {
   searchParams: Promise<{ categoryName: string }>;
@@ -18,17 +19,22 @@ const CategoryPage = async ({ searchParams }: CategoryPageParams) => {
   return (
     <>
       <Header />
-      <section className="flex flex-col gap-4 px-5 pt-6">
-        <h2 className="text-lg font-semibold">{category.name}</h2>
-        {category.products.map((product) => (
-          <ProductItem
-            product={{
-              ...product,
-              originalPrice: Number(product.originalPrice),
-            }}
-            key={product.id}
-          />
-        ))}
+      <div className="hidden pb-10 pt-5 md:block">
+        <Separator />
+      </div>
+      <section className="px-5 pt-6 md:px-32 md:pb-6 md:pt-0">
+        <h2 className="pb-6 text-lg font-semibold">{category.name}</h2>
+        <div className="flex flex-col gap-4 md:flex-row md:gap-6">
+          {category.products.map((product) => (
+            <ProductItem
+              product={{
+                ...product,
+                originalPrice: Number(product.originalPrice),
+              }}
+              key={product.id}
+            />
+          ))}
+        </div>
       </section>
     </>
   );
